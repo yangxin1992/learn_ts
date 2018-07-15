711aba1df7b886767a97a2e9270bd747391d8e9c
xincyang <yxwymail@163.com>
2018-07-15 22:29:51 +0800

    类型

---
+++ example004/types.ts
@@ -0,0 +1,21 @@
+class o {
+  name: string = 'default';
+}
+
+class a extends o {
+  a: string; 
+}
+
+class b extends o {
+  // b: string;
+}
+
+class c extends o {
+  // c: string;
+}
+
+const test = [ new a(), new b(), new c() ];
+
+if (test[0] instanceof a) {
+  (test[0] as a).a;
+}
